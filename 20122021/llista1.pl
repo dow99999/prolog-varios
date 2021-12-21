9b43a718bde93ba69bee9e52c6226ea9f2996ab5
Diego Muñoz Jorquera <munoz.jorquera.diego@alumnat.copernic.cat>
2021-12-21 12:49:26 +0100

    Update llista1.pl

--- 20122021/llista1.pl
+++ 20122021/llista1.pl
@@ -1,13 +1,18 @@
+% comprova si un element es dinsn una llista
+% element_de(X, [X|Cua]).
+% element_de(X, [_|Cua]) :- element_de(X, Cua).
+
 % té èxit si Llista és una llista que només conté as com a elements
-tot_as([]).
+tot_as([a]) :- !.
 tot_as([a|Cua]) :- tot_as(Cua).
 
 
 % OutList s'obté de InList substituint totes les as d'InList per b, totes les bs per cs i totes les cs per as.
 substi_a_b_c([], []).
-substi_a_b_c([a|CuaEntrada], [b|CuaSortida]) :- substi_a_b_c(CuaEntrada, CuaSortida).
-substi_a_b_c([b|CuaEntrada], [c|CuaSortida]) :- substi_a_b_c(CuaEntrada, CuaSortida).
-substi_a_b_c([c|CuaEntrada], [a|CuaSortida]) :- substi_a_b_c(CuaEntrada, CuaSortida).
+substi_a_b_c([a|CuaEntrada], [b|CuaSortida]) :- substi_a_b_c(CuaEntrada, CuaSortida), !.
+substi_a_b_c([b|CuaEntrada], [c|CuaSortida]) :- substi_a_b_c(CuaEntrada, CuaSortida), !.
+substi_a_b_c([c|CuaEntrada], [a|CuaSortida]) :- substi_a_b_c(CuaEntrada, CuaSortida), !.
+substi_a_b_c([X|CuaEntrada], [X|CuaSortida]) :- substi_a_b_c(CuaEntrada, CuaSortida).
 
 
 % Lng és el nombre d'elements de la llista Llista.
@@ -24,4 +29,17 @@ suma_un([V|CuaE], [O|CuaS]) :- suma_un(CuaE, CuaS), O is V+1.
 % que sigui cert quan la llista d'entrada Llista conté un 0.
 te_un_zero([0]).
 te_un_zero([Value|Cua]) :- te_un_zero(Cua); Value is 0, !.
+
+max([X], X) :- !.
+
+max([V|Cua], Max) :- 
+  max(Cua, Aux),
+  V > Aux,
+  Max is V,
+  !.
+
+max([V|Cua], Max) :- 
+  max(Cua, Aux),
+  V =< Aux,
+  Max is Aux.
 
