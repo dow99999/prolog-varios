3f6d69971fa475d5a6bda6db8acbbbf3b8ec02ec
Diego Muñoz Jorquera <munoz.jorquera.diego@alumnat.copernic.cat>
2021-12-21 13:09:28 +0100

    Create mini_sistema_de_clasificacio.pl

---
+++ 20122021/mini_sistema_de_clasificacio.pl
@@ -0,0 +1,18 @@
+%%
+%% Dos apartats:
+%%  Base de Fets    Modificable
+%%
+%%  Base de regles  Estable
+%%
+%% Problemes analitics (treure una resposta a partir d'unes dades)
+%%
+%% Solucions a partir de limitacions
+%%
+%% diagnosticar == classificar == identificar
+%%
+%%
+%% Borrar fets:     retract            undo
+%%
+%% bigml.com
+%%
+%%
